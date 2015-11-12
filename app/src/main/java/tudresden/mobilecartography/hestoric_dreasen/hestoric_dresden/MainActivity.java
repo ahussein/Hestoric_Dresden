b67b61e2e7ed2041bac9822aaf18e2b55303a33e
Abdelrahman Hussein <abdelrahman.hussein@hgst.com>
2015-11-12 16:20:55 +0100

    Initial commit

---
+++ app/src/main/java/tudresden/mobilecartography/hestoric_dreasen/hestoric_dresden/MainActivity.java
@@ -0,0 +1,13 @@
+package tudresden.mobilecartography.hestoric_dreasen.hestoric_dresden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
