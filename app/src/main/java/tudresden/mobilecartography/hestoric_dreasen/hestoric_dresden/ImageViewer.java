890f881abfc73688f7e001c951f51cf671627478
Abdelrahman Hussein <abdelrahman.hussein@hgst.com>
2016-01-25 18:03:19 +0100

    add image indicator

--- app/src/main/java/tudresden/mobilecartography/hestoric_dreasen/hestoric_dresden/ImageViewer.java
+++ app/src/main/java/tudresden/mobilecartography/hestoric_dreasen/hestoric_dresden/ImageViewer.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.viewpagerindicator.CirclePageIndicator;
+
 public class ImageViewer extends AppCompatActivity {
 
     @Override
@@ -19,6 +21,14 @@ public class ImageViewer extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
         ImageAdapter adapter = new ImageAdapter(this, attraction);
         viewPager.setAdapter(adapter);
+        CirclePageIndicator indicator = (CirclePageIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(viewPager);
+        final float density = getResources().getDisplayMetrics().density;
+        // Set circle indicator radius
+        indicator.setRadius(5 * density);
+        indicator.setFillColor(R.color.colorPrimaryDark);
+        indicator.setStrokeColor(R.color.text_color);
+
     }
 
     @Override
